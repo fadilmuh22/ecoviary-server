7c4796e9b65b7e21812a6b2502e17b1439f153ed
fadilmuh22 <fadilmuh2002@gmail.com>
2024-02-07 16:45:13 +0700

    chore: change default port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { watchAutomations } from "./scheduler";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 const _ = watchAutomations();
 
